81e87994e6ca76e70dcdda2506b183ac42fa86b7
sadeeqdev <boskitechh@gmail.com>
2024-04-18 08:47:43 +0100

    fix: db collection

--- connection/mongo.conn.js
+++ connection/mongo.conn.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
-
 module.exports = async () => {
-    const dbConn = mongoose.connection;
-    dbConn
-        .on("connected", () => {
-            console.log(" connected to mongo");
-        })
-        .on("error", (error) => {
-            console.log(`error connecting to mongo >> ${error.message}`);
-        })
-        .on("disconnected", () => {
-            console.log("disconnected from mongo");
-            setTimeout(async () => {
-                console.log("reconnecting to mongo");
-                await mongoose.connect("mongodb+srv://teekay014:S2MNKitOoAFVGDfg@cluster0.l4oidba.mongodb.net/?retryWrites=true&w=majority")
-            }, 5000);
-        })
-    await mongoose.connect("mongodb+srv://teekay014:S2MNKitOoAFVGDfg@cluster0.l4oidba.mongodb.net/?retryWrites=true&w=majority")
-}
+  const dbConn = mongoose.connection;
+  dbConn
+    .on("connected", () => {
+      console.log(" connected to mongo");
+    })
+    .on("error", (error) => {
+      console.log(`error connecting to mongo >> ${error.message}`);
+    })
+    .on("disconnected", () => {
+      console.log("disconnected from mongo");
+      setTimeout(async () => {
+        console.log("reconnecting to mongo");
+        await mongoose.connect(
+          "mongodb+srv://sadeeqdev:seeman11@cluster0.ek6rkci.mongodb.net/?retryWrites=true&w=majority"
+        );
+      }, 5000);
+    });
+  await mongoose.connect(
+    "mongodb+srv://sadeeqdev:seeman11@cluster0.ek6rkci.mongodb.net/robdb?retryWrites=true&w=majority"
+  );
+};
