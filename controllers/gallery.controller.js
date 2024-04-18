c9f30e6f3f0578a57e499c382150fb8de4008472
sadeeqdev <boskitechh@gmail.com>
2024-04-18 09:54:49 +0100

    fix: upload files to clodinary

--- controllers/gallery.controller.js
+++ controllers/gallery.controller.js
@@ -1,3 +1,4 @@
+const cloudinary = require("cloudinary").v2;
 const galleryModel = require("../models/gallery.model");
 
 class GalleryController {
@@ -5,14 +6,45 @@ class GalleryController {
 
   async addGallery(data, images) {
     try {
+      cloudinary.config({
+        cloud_name: "dyngfmfiz",
+        api_key: "179489529593872",
+        api_secret: "UcEaI_lZw_WjxOYKXX8r3yRrYf0",
+      });
       const galleryData = [];
+      let fileUrl = "";
       for (const image of images) {
+        if (image.mimetype.startsWith("image/")) {
+          await cloudinary.uploader.upload(
+            image.path,
+            { public_id: image.filename },
+            function (error, result) {
+              fileUrl = result?.url;
+              error && console.log(error);
+            }
+          );
+        } else {
+          await cloudinary.uploader
+            .upload(image.path, {
+              resource_type: "video",
+              public_id: image.filename,
+            })
+            .then((data) => {
+              console.log(data);
+              fileUrl = data?.url;
+            })
+            .catch((err) => {
+              console.err(err);
+            });
+        }
+
         const newData = {
           name: data.name,
           file: image.filename,
-          path: image.path,
+          path: fileUrl,
           type: image.mimetype,
         };
+        console.log(newData);
         const gallery = new galleryModel(newData);
         const result = await gallery.save();
         galleryData.push(result);
